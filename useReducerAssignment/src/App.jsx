b621b17cfbdd41d534109670dd94429cc514ad53
VKS0104 <vedsingh0104@gmail.com>
2026-08-03 22:34:29 +0530

    Question3 Completed

--- useReducerAssignment/src/App.jsx
+++ useReducerAssignment/src/App.jsx
@@ -50,35 +50,80 @@
 
 // export default App
 
-import React, { useReducer } from 'react'
+// import React, { useReducer } from 'react'
 
-const App = () => {
-  
-  const [state,dispatch] = useReducer(reduser,"red")
-  function reduser(state,action)
-  {
-    if(action.type === "color")
-    {
-      if(state == "red")
-      {
-        return "yellow"
-      }
-      else if(state == "yellow")
-      {
-        return "green"
-      }
-      else if(state == "green")
-      {
-        return "red"
-      }
-    }
-  }
-  return (
-    <div style={{backgroundColor:state, height:"100vh"}}>
-      <button onClick={()=> dispatch({type:"color"})}>Click</button>
-    </div>
-  )
-}
+// const App = () => {
 
-export default App
+//   const [state,dispatch] = useReducer(reduser,"red")
+//   function reduser(state,action)
+//   {
+//     if(action.type === "color")
+//     {
+//       if(state == "red")
+//       {
+//         return "yellow"
+//       }
+//       else if(state == "yellow")
+//       {
+//         return "green"
+//       }
+//       else if(state == "green")
+//       {
+//         return "red"
+//       }
+//     }
+//   }
+//   return (
+//     <div style={{backgroundColor:state, height:"100vh"}}>
+//       <button onClick={()=> dispatch({type:"color"})}>Click</button>
+//     </div>
+//   )
+// }
 
+// export default App
+
+
+// import React, { useReducer } from 'react'
+
+// const App = () => {
+//   const initialValue = {
+//     item: "",
+//     cart: [],
+//   }
+//   const [state, dispatch] = useReducer(reduser, initialValue)
+
+//   function reduser(state, action) {
+//     if (action.type === "input") {
+//       return {
+//         ...state,
+//         item: action.payload
+//       }
+//     }
+//     else if (action.type === "add") {
+//       if (state.item.trim() === "") {
+//         return state;
+//       }
+//       return {
+//         ...state,
+//         cart: [...state.cart, state.item],
+//         item: "",
+//       }
+//     }
+//   }
+//   return (
+//     <div>
+//       <input type="text" value={state.item} onChange={(e) => dispatch({ type: "input", payload: e.target.value })} />
+//       <button onClick={() => dispatch({ type: "add" })}>Add</button>
+
+//       {
+//         state.cart.map((e) => {
+//           return (
+//             <div>{e}</div>
+//           )
+//         })
+//       }
+//     </div>
+//   )
+// }
+
+// export default App
